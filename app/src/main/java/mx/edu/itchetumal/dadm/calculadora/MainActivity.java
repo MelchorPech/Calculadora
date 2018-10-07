6895a1edd505e35618aecd11d07b3a1dcdcbe587
MelchorPech <15390232@itchetumal.edu.mx>
2018-10-07 18:41:34 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/itchetumal/dadm/calculadora/MainActivity.java
@@ -0,0 +1,217 @@
+package mx.edu.itchetumal.dadm.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.app.Activity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btn1,btnBorrar,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnSuma,btnIgual;
+    EditText edtResul;
+    String aux,aux3;
+    String operador, reserva;
+    String mostrar="";
+    int resultado,aux2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //programa suma
+        btnSuma= findViewById(R.id.btnSumar);
+        btnSuma.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           reserva = edtResul.getText().toString();
+                                           operador = "+";
+                                           edtResul.setText("");
+                                       }
+                                   }
+
+        );
+        //Programar el Click para el boton de borrar
+        btnBorrar = findViewById(R.id.btnBorrar);
+        btnBorrar.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View v) {
+                                             mostrar = "";
+                                             edtResul.setText(mostrar);
+                                             reserva = "";
+                                             operador = "";
+                                         }
+                                     }
+
+        );
+        //Programa para la igual
+        btnIgual= findViewById(R.id.btnIgual);
+        btnIgual.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            mostrar = edtResul.getText().toString();
+                                            mostrar = mostrar + "1";
+                                            if(operador.equals("+")){
+                                                resultado = Integer.parseInt(reserva) + Integer.parseInt (edtResul.getText().toString());
+                                                edtResul.setText(String.valueOf(resultado));
+                                            }
+
+                                        }
+                                    }
+
+        );
+        //Programa para insertar "1" a la pantalla de resultado
+        edtResul =(EditText) findViewById(R.id.edtResultado);
+        btn1= findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edtResul.getText().toString().compareTo("0")==0){
+                    edtResul.setText("1");
+                }else
+                     mostrar = edtResul.getText().toString();
+                     mostrar = mostrar + "1";
+                     edtResul.setText(mostrar);
+            }
+        });
+        //Programa para insertar "2" a la pantalla de resultado
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if(edtResul.getText().toString().compareTo("0")==0){
+                                            edtResul.setText("2");
+                                        }else
+                                            mostrar = edtResul.getText().toString();
+                                        mostrar = mostrar + "2";
+                                        edtResul.setText(mostrar);
+                                    }
+                                }
+
+        );
+        //Programa para insertar "3" a la pantalla de resultado
+        btn3 = findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if(edtResul.getText().toString().compareTo("0")==0){
+                                            edtResul.setText("3");
+                                        }else
+                                            mostrar = edtResul.getText().toString();
+                                        mostrar = mostrar + "3";
+                                        edtResul.setText(mostrar);
+                                    }
+                                }
+
+        );
+        //Programa para insertar "4" a la pantalla de resultado
+        btn4 = findViewById(R.id.btn4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if(edtResul.getText().toString().compareTo("0")==0){
+                                            edtResul.setText("4");
+                                        }else
+                                            mostrar = edtResul.getText().toString();
+                                        mostrar = mostrar + "4";
+                                        edtResul.setText(mostrar);
+                                    }
+                                }
+
+        );
+        //Programa para insertar "5" a la pantalla de resultado
+        btn5 = findViewById(R.id.btn5);
+        btn5.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if(edtResul.getText().toString().compareTo("0")==0){
+                                            edtResul.setText("5");
+                                        }else
+                                            mostrar = edtResul.getText().toString();
+                                        mostrar = mostrar + "5";
+                                        edtResul.setText(mostrar);
+                                    }
+                                }
+
+        );
+        //Programa para insertar "6" a la pantalla de resultado
+        btn6 = findViewById(R.id.btn6);
+        btn6.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if(edtResul.getText().toString().compareTo("0")==0){
+                                            edtResul.setText("6");
+                                        }else
+                                            mostrar = edtResul.getText().toString();
+                                        mostrar = mostrar + "6";
+                                        edtResul.setText(mostrar);
+                                    }
+                                }
+
+        );
+        //Programa para insertar "7" a la pantalla de resultado
+        btn7 = findViewById(R.id.btn7);
+        btn7.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if(edtResul.getText().toString().compareTo("0")==0){
+                                            edtResul.setText("7");
+                                        }else
+                                            mostrar = edtResul.getText().toString();
+                                        mostrar = mostrar + "7";
+                                        edtResul.setText(mostrar);
+                                    }
+                                }
+
+        );
+        //Programa para insertar "8" a la pantalla de resultado
+        btn8 = findViewById(R.id.btn8);
+        btn8.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if(edtResul.getText().toString().compareTo("0")==0){
+                                            edtResul.setText("8");
+                                        }else
+                                            mostrar = edtResul.getText().toString();
+                                        mostrar = mostrar + "8";
+                                        edtResul.setText(mostrar);
+                                    }
+                                }
+
+        );
+        //Programa para insertar "9" a la pantalla de resultado
+        btn9 = findViewById(R.id.btn9);
+        btn9.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if(edtResul.getText().toString().compareTo("0")==0){
+                                            edtResul.setText("9");
+                                        }else
+                                            mostrar = edtResul.getText().toString();
+                                        mostrar = mostrar + "9";
+                                        edtResul.setText(mostrar);
+                                    }
+                                }
+
+        );
+        //Programa para insertar "0" a la pantalla de resultado
+        btn0 = findViewById(R.id.btn0);
+        btn0.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                     public void onClick(View v) {
+                                          if(edtResul.getText().toString().compareTo("0")==0){
+                                             edtResul.setText("0");
+                                          }else
+                                             mostrar = edtResul.getText().toString();
+                                             mostrar = mostrar + "0";
+                                             edtResul.setText(mostrar);
+            }
+        });
+
+    }
+
+}
